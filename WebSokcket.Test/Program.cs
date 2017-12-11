05fc69710602d26032394f7d6a167d89c8d915c5
moran2017 <31378556+moran2017@users.noreply.github.com>
2017-12-11 11:34:34 -0600

    Initial commit

---
+++ WebSokcket.Test/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WebSokcket.Test
+{
+    class Program
+    {
+        static Queue<String> Cola;
+        static void Main(string[] args)
+        {
+            Cola = new Queue<String>();
+            Thread hiloPrincipal = new Thread(Procesar);
+            hiloPrincipal.Start();
+            Thread hiloProcesar = new Thread(ProcesarInformacion);
+            hiloProcesar.Start();
+        }
+
+        static Byte[] ConverAsciiToEbcdic(Byte[] asciiData)
+        {
+            Encoding ascii = Encoding.ASCII;
+            Encoding ebcdic = Encoding.GetEncoding("IBM037");
+            return Encoding.Convert(ascii, ebcdic, asciiData);
+        }
+
+        static void ProcesarInformacion()
+        {
+            while (Cola.Count != 0)
+            {
+                String strTrama = Cola.Dequeue();
+                Byte[] arrByteRequest = Encoding.ASCII.GetBytes(strTrama);
+                Byte[] arrByteEbcdic = ConverAsciiToEbcdic(arrByteRequest);
+                Console.WriteLine(BitConverter.ToString(arrByteEbcdic));
+            }
+
+        }
+
+        static void Procesar()
+        {
+
+            while (true)
+            {
+                try
+                {
+                    String strRequest = "1804";
+                    Cola.Enqueue(strRequest);
+                    Thread.Sleep(500);
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
